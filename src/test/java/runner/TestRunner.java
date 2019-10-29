de420b52f43696e56440d829a656d4b2a73fdf4f
Nishant <nishant.s.aux@go-jek>
2019-10-29 23:57:48 +0530

    first commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/"
+        , glue = {"src/test/java/steps/"}
+)
+
+public class TestRunner {
+
+}
